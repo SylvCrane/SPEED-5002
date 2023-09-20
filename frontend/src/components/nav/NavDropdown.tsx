44a0a6076ef190ea90440635bed1db24727223a9
SylvCrane <104751218+SylvCrane@users.noreply.github.com>
2023-09-20 22:55:03 +1200

    Updating nav.module references

--- frontend/src/components/nav/NavDropdown.tsx
+++ frontend/src/components/nav/NavDropdown.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Nav.module.scss";
+import styles from "../Nav.module.scss";
 
 type Props = {
   children?: React.ReactNode;
--- frontend/src/components/nav/NavItem.tsx
+++ frontend/src/components/nav/NavItem.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import React from "react";
-import styles from "./Nav.module.scss";
+import styles from "../Nav.module.scss";
 
 type Props = {
   route?: string;
