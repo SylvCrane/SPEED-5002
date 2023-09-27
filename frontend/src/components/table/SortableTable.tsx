cec3afbac6fa1558e154e4d385a0b6f1ed14448d
llwong03 <lachlan.ljwong@gmail.com>
2023-09-28 09:33:50 +1300

    Sortable table

--- frontend/src/components/table/SortableTable.tsx
+++ frontend/src/components/table/SortableTable.tsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './SortableTable.module.scss'; // Import styles
 
 interface SortableTableProps {
   headers: { key: string; label: string }[];
   data: any[];
 }
 
-const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => (
-  <table>
-    <thead>
-      <tr>
-        {headers.map((header) => (
-          <th key={header.key}>{header.label}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((row, i) => (
-        <tr key={i}>
+const SortableTable: React.FC<SortableTableProps> = ({ headers, data }) => {
+  const [sortConfig, setSortConfig] = useState<{
+    key: string | null;
+    direction: "ascending" | "descending";
+  }>({
+    key: null,
+    direction: "ascending",
+  });
+
+  const sortedData = React.useMemo(() => {
+    let sortableData = [...data];
+    if (sortConfig.key) {
+      sortableData.sort((a, b) => {
+        if (a[sortConfig.key!] < b[sortConfig.key!]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key!] > b[sortConfig.key!]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableData;
+  }, [data, sortConfig]);
+
+  const handleSort = (key: string) => {
+    let direction: "ascending" | "descending" = "ascending";
+    if (
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
           {headers.map((header) => (
-            <td key={header.key}>{row[header.key]}</td>
+            <th className={styles.th} key={header.key}>
+              <span className={styles.sortableLabel} onClick={() => handleSort(header.key)}>
+                {header.label}
+              </span>
+            </th>
           ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {sortedData.map((row, i) => (
+          <tr key={i}>
+            {headers.map((header) => (
+              <td className={styles.td} key={header.key}>{row[header.key]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default SortableTable;
